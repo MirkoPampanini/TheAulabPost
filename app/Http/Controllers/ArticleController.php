ba53f21e8b054072da449c59f0e190c8acfd245c
Mirko Pampanini <mirkopampanini01@gmail.com>
2024-09-23 23:24:33 +0200

    storage creato

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Article;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ArticleController extends Controller
 {
@@ -28,7 +29,23 @@ class ArticleController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'title' =>'required|unique:articles|min:5',
+            'subtitle' => 'required|min:5' ,
+            'body' => 'required|min:10' ,
+            'image' => 'required|image',
+            'category' => 'required',
+        ]);
+
+        $article = Article::create([
+            'title' => $request->title,
+            'subtitle'=>$request->subtitle,
+            'body' =>$request->body,
+            'image'=>$request->file('image')->store('public/images'),
+            'category_id'=> $request->category,
+            'user_id' => Auth::user()->id,
+        ]);
+        return redirect(route('homepage'))->with('message' , 'Articolo creato con successo');
     }
 
     /**
